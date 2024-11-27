f0d83f00667879296340e1c68cddf34ac7c792d5
aymene <aymenebd@gmail.com>
2024-11-27 01:55:58 +0100

    first commit

---
+++ lib/login.page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginAlternativeViewState();
+}
+
+class _LoginAlternativeViewState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "This is the title lfo9",
+          style:
+              TextStyle(color: Theme.of(context).indicatorColor, fontSize: 24),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Connexion',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 35),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Nom d\'utilisateur',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 35),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              child: const Text('Se connecter'),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Mot de passe oublié?'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
